ac61fa90c9baf33967b7f8377a27918ad80c8b4c
Imbeshat <md.imbeshat@gmail.com>
2023-03-11 17:35:51 +0530

    minor edit

--- MiniProjects/side-zero-router/src/Components/Footer.js
+++ MiniProjects/side-zero-router/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<footer className="text-center bg-slate-900 text-white bottom-0 fixed h-10 w-full">
-			<p className="my-2">Copyright @none</p>
+			<p className="my-2">Copyright @no copyright</p>
 		</footer>
 	);
 };
