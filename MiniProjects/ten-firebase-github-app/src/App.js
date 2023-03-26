0d631808adfaaf48c57fc18d7539281510d34d17
Imbeshat <md.imbeshat@gmail.com>
2023-03-27 02:48:01 +0530

    added routes, context and components

--- MiniProjects/ten-firebase-github-app/src/App.js
+++ MiniProjects/ten-firebase-github-app/src/App.js
@@ -1,11 +1,38 @@
+import React, { useState } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+import { UserContext } from "./context/UserContext";
+
+const App = () => {
+	const [user, setUser] = useState(null);
+
 	return (
-		<div className="App">
-			<h1>Test</h1>
-		</div>
+		<Router>
+			<ToastContainer />
+			<UserContext.Provider value={{ user, setUser }}>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/signin" element={<Signin />} />
+					<Route exact path="/signup" element={<Signup />} />
+					<Route exact path="*" element={<PageNotFound />} />
+				</Routes>
+			</UserContext.Provider>
+		</Router>
 	);
-}
+};
 
 export default App;
